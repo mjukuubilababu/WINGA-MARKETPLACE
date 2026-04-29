0a9fdd29854b972474fc2da0c3d8732b9f4b09b8
Willard Mmbando <willardmmbando@gmail.com>
2026-04-29 17:59:18 +0300

    Fix production URL routing

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-const BUILD_VERSION = "20260429003202";
+const BUILD_VERSION = "20260429145208";
 const CACHE_PREFIX = "winga-shell";
 const CACHE_NAME = `${CACHE_PREFIX}-${BUILD_VERSION}`;
 const IMAGE_CACHE_PREFIX = "winga-images";
