cf2a198845723c492a2ec9f18b49850c478d1d73
Willard Mmbando <willardmmbando@gmail.com>
2026-07-19 15:28:34 +0300

    Route production API through same origin

--- winga-config.js
+++ winga-config.js
@@ -2,7 +2,7 @@ const WINGA_PROTOCOL = window.location.protocol;
 const WINGA_HOST = window.location.hostname;
 const WINGA_IS_FILE_MODE = WINGA_PROTOCOL === "file:";
 const WINGA_IS_LOCAL_WEB = WINGA_HOST === "localhost" || WINGA_HOST === "127.0.0.1";
-const WINGA_PRODUCTION_API_BASE_URL = "https://winga-pflp.onrender.com/api";
+const WINGA_PRODUCTION_API_BASE_URL = "/api";
 
 const WINGA_CONFIG_OVERRIDE = window.__WINGA_CONFIG_OVERRIDE__ || {};
 const WINGA_DEFAULT_CONFIG = {
